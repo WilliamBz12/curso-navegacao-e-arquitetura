1c1dd625c1733ab989480fc9733472b3b9f59981
Williambz12 <williamsilva754@gmail.com>
2025-06-25 20:11:48 -0300

    first commit

---
+++ lib/utils/app_config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  // Cores do tema
+  static const Color backgroundColor = Color(0xFF021123);
+  static const Color buttonColor = Color(0xFFB872FF);
+
+  // Imagens
+  static const String logoImage = 'assets/logo.png';
+  static const String homeImage = 'assets/home.png';
+
+  // Mensagem rodapé
+  static const String footerText =
+      'Projeto fictício e sem fins comerciais.\nDesenvolvido por Alura.';
+}
